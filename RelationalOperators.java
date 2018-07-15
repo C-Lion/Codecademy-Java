7b0e78d486cf79b4c255fbdd3242a5dbf7cefb78
C-Lion <esstepansky@gmail.com>
2018-07-16 01:24:52 +0300

    add RelationalOperators.java including  equality examples

---
+++ RelationalOperators.java
@@ -0,0 +1,20 @@
+/****************************************
+*             							*
+*	RelationalOperators.java           	*
+*	Codecademy                          *
+*	Sunday July 15, 2018  			 	*
+*									 	*
+*****************************************/
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+		
+		//Relational Operator example:
+		System.out.println(18 >= 21);
+
+		//Equality Operator example:
+		System.out.println('A' == -2);
+		
+	}
+	
+}

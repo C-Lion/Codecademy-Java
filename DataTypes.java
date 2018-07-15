f15b3babe0edea86a1de4dd2ef9fd83e09c7d096
C-Lion <esstepansky@gmail.com>
2018-07-16 00:28:49 +0300

    Add Boolean and char data types

--- DataTypes.java
+++ DataTypes.java
@@ -8,7 +8,13 @@
 public class DataTypes {
 	public static void main(String[] args) {
 
+		//Data type I: int: 
 		System.out.println(42);
+		//Data type II: Boolean:
+		System.out.println(true);
+		//Data type III:
+		System.out.println('M');
+
 		
 	}
 

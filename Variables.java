52e7ffa5d7fb6199b9714f1b91bfea677f8e3317
C-Lion <esstepansky@gmail.com>
2018-07-16 00:37:35 +0300

    add completed Variables.java file

---
+++ Variables.java
@@ -0,0 +1,21 @@
+/****************************************
+*             							*
+*	Variables.java                    	*
+*	Codecademy                          *
+*	Sunday July 15, 2018  			 	*
+*									 	*
+*****************************************/
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		int myNumber = 42;
+		boolean isFun = true;
+		char movieRating = 'A';
+
+		System.out.println(myNumber);
+		System.out.println(isFun);
+		System.out.println(movieRating);
+		
+	}
+}

949b3ae33b695bdeda458096281ae6c61b0bad14
C-Lion <esstepansky@gmail.com>
2018-07-16 00:52:57 +0300

    add WhiteSpace.java file and add comments examples"

---
+++ WhiteSpace.java
@@ -0,0 +1,28 @@
+/****************************************
+*             							*
+*	WhiteSpace.java                    	*
+*	Codecademy                          *
+*	Sunday July 15, 2018  			 	*
+*									 	*
+*****************************************/
+
+public class WhiteSpace {
+	public static void main(String[] args) {
+		
+		//This layout makes proper use of whitespace. 
+		boolean isFormatted = false;
+		System.out.println(isFormatted);
+
+		//This is a single line comment. 
+
+		/*Comments can also be used "comment out code" so the compiler
+		will skip over it. 
+		This is usefule for troubleshooting errors.
+		The following code is commented out: 
+		*/
+
+		//System.out.println("Noise!");
+
+
+	}
+}

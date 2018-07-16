2d3794da372a2e8e28b4ae80e14d6220b4783895
C-Lion <esstepansky@gmail.com>
2018-07-16 04:57:58 +0300

    Add If example to IfElseIfTernary.java

---
+++ IfElseIfTernary.java
@@ -0,0 +1,17 @@
+/****************************************
+*             							*
+*	IfElseIfTernary.java               	*
+*	Codecademy                          *
+*	Monday July 16, 2018   				*
+*									 	*
+*****************************************/
+public class IfElseIfTernary {
+	public static void main(String[] args) {
+		
+		//If:
+		int passcode = 42;
+		if (passcode == 42) {
+			System.out.println("Access granted.");
+		}
+	}
+}

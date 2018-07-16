5b88995432aede7a0d45a42ce96a669d18d8e4d7
C-Lion <esstepansky@gmail.com>
2018-07-16 05:50:52 +0300

    Add the second switch case example

--- SwitchAndGeneralizationsB.java
+++ SwitchAndGeneralizationsB.java
@@ -24,7 +24,42 @@ public class SwitchAndGeneralizationsB {
 
 		default: 
 			System.out.println("Messi is in position...");
+	}
+
+//GeneralizationsB:
+		// set tricky to the same value as:( 3 >= 3 && !(true || true))
+		boolean tricky = false;
+
+		//This if stmt changed to make the else block run: 
+		//From: if !(2015 < 2016) {
+		//To:
+		if (2015 > 2016) {
+		
+		 	System.out.println("Stuck in the past...");
+		
+		} else {
+			
+			System.out.println("Upgraded to the future!");
+
 		}
 
-	}
+//Set the value of int so that the case of the Brooklyn train runs:
+		int subwayTrain = 5;
+
+			switch (subwayTrain){
+
+				case 1 : System.out.println("This is a South Ferry bound train!");
+						break;
+
+				case 5 : System.out.println("This is a Brooklyn bound train!");
+						break;
+
+				case 7 : System.out.println("This is a Queens bound train!");
+						break;
+
+				default: 
+					System.out.println("I'm not sure where that train goes...");
+
+			}
+		}
 }

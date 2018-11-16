d17d645858c48700057dda62558254e625fd8335
C-Lion <esstepansky@gmail.com>
2018-11-16 15:50:22 +0200

    add Calculator file

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+/****************************************
+*                                       *
+* Calculator.java                       *
+* Codecademy                            *
+* Monday July 23, 2018                  *
+*                                       *
+* A program that uses basic arithmetic  *
+* operation to calculate the sum, 	    *
+* difference, product, quotient, and    *
+* remainder of two integers.            *
+*                                       *
+*****************************************/
+import java.io.Console;
+
+
+
+
